ad170ec249dcd47d224ae4814c4d31fd0efe9962
unknown <Shanereeve@gmail.com>
2021-03-23 21:38:48 -0600

    Lesson 163 of Angular Course

--- src/app/recipes/recipes-list/recipes-item/recipes-item.component.ts
+++ src/app/recipes/recipes-list/recipes-item/recipes-item.component.ts
@@ -9,7 +9,8 @@ import { RecipeService } from '../../recipe.service';
 })
 export class RecipesItemComponent implements OnInit {
 
-  @Input() recipe: Recipe
+  @Input() recipe: Recipe;
+  @Input() index: number;
 
   constructor(private recipeService: RecipeService) { }
 
