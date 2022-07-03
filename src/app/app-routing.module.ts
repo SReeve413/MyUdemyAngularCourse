09b7e389755743853186bc18787951105948c999
Shane Reeve <shanereeve@gmail.com>
2022-07-03 12:10:30 -0600

    285: Resolving Data Before Loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,14 +7,15 @@ import { RecipesDetailComponent } from "./recipes/recipes-detail/recipes-detail.
 import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
 
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+import { RecipesResolverService } from "./recipes/recipes-resolver.service";
 
 const appRoutes: Routes = [
     { path: '',  pathMatch: 'full', redirectTo: '/recipes'},
     { path: 'recipes', component: RecipesComponent, children: [
         {path: '', pathMatch: 'full', component: RecipeStartComponent },
         {path: 'new',  component: RecipeEditComponent},
-        {path: ':id',  component: RecipesDetailComponent},
-        {path: ':id/edit',  component: RecipeEditComponent},
+        {path: ':id',  component: RecipesDetailComponent, resolve: [RecipesResolverService]},
+        {path: ':id/edit',  component: RecipeEditComponent, resolve: [RecipesResolverService]},
     ]},
     { path: 'shopping-list', component: ShoppingListComponent },
 
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
-import { map } from 'rxjs/operators'
+import { map, tap } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root',
@@ -28,17 +28,19 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http.get<Recipe[]>(this.url + 'recipes.json')
-    .pipe(map(recipes => {
+    return this.http.get<Recipe[]>(this.url + 'recipes.json')
+    .pipe(
+      map(recipes => {
       return recipes.map(recipe => {
         return {
           ...recipe,
           ingridents: recipe.ingredients ? recipe.ingredients : []
         }
       })
-    }))
-    .subscribe(recipes => {
+    }),
+    tap(recipes => {
       this.recipeService.setRecipes(recipes);
     })
+    )
   }
 }
