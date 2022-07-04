cce68aca8ed8c164517e410ac51298c9527c8799
Shane Reeve <shanereeve@gmail.com>
2022-07-04 10:19:27 -0600

    295: Sending the Signup Request

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,22 +1,38 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
-  templateUrl: './auth.component.html'
+  templateUrl: './auth.component.html',
 })
 export class AuthComponent {
+  isLoginMode = true;
 
-isLoginMode = true;
+  constructor(private authService: AuthService) {}
 
-onSwitchMode(){
-  this.isLoginMode = !this.isLoginMode;
-}
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
 
-onSubmit(form: NgForm){
-  console.log(form.value)
-  form.reset();
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
 
-}
+    if (this.isLoginMode) {
+      //...
+    } else {
+      this.authService.signUp(email, password).subscribe(
+        (responseData) => {
+          console.log(responseData);
+        },
+        (error) => console.log(error)
+      );
+    }
 
+    form.reset();
+  }
 }
