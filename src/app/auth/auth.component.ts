043f18164925193808956491b5009e06a42307f3
Shane Reeve <shanereeve@gmail.com>
2022-07-05 22:42:25 -0600

    298: Seding Login Request

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AuthService } from './auth.service';
+import { Observable } from 'rxjs';
+import { AuthResponseData, AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -23,25 +24,30 @@ export class AuthComponent {
     }
     const email = form.value.email;
     const password = form.value.password;
+
+    let authObs: Observable<AuthResponseData>
+
     this.isLoading = true;
     if (this.isLoginMode) {
       //...
-      this.isLoading = false;
+      authObs = this.authService.login(email, password);
+
 
     } else {
-      this.authService.signUp(email, password).subscribe(
-        (responseData) => {
-          console.log(responseData);
-          this.isLoading = false;
-
-        },
-        (errorMessage) => {
-          console.log(errorMessage)
-          this.error = errorMessage;
-          this.isLoading = false;
-        }
-      );
+      authObs = this.authService.signUp(email, password);
     }
+
+    authObs.subscribe(
+      (responseData) => {
+        console.log(responseData);
+        this.isLoading = false;
+      },
+      (errorMessage) => {
+        console.log(errorMessage)
+        this.error = errorMessage;
+        this.isLoading = false;
+      }
+    );
 
     form.reset();
   }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,13 +4,14 @@ import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 
 
-interface AuthResponseData {
+export interface AuthResponseData {
   kind: string,
   idToken: string,
   email: string,
   refreshToken: string,
   expiresIn: string,
-  localId: string
+  localId: string,
+  registered?: boolean
 }
 
 @Injectable({
@@ -40,5 +41,14 @@ export class AuthService {
       return throwError(errorMessage);
 
     }));
+  }
+
+  login(email: string, password: string) {
+    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBIuD7iNQOG8bvrGAjLvKUNdRTiLC7Et0k',
+    {
+      email: email,
+      password: password,
+      secureToken: true,
+    })
   }
 }
