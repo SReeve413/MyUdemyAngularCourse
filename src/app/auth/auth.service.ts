ee2c4c5f3c2511e1a6ff19cbc4b97ec2c438c494
Shane Reeve <shanereeve@gmail.com>
2022-07-06 21:25:17 -0600

    Creating & Storing the User Data

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,52 +1,69 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError } from 'rxjs/operators';
-import { throwError } from 'rxjs';
-
+import { catchError, tap } from 'rxjs/operators';
+import { Subject, throwError } from 'rxjs';
+import { User } from './user.model';
 
 export interface AuthResponseData {
-  kind: string,
-  idToken: string,
-  email: string,
-  refreshToken: string,
-  expiresIn: string,
-  localId: string,
-  registered?: boolean
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
 }
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  user = new Subject<User>();
   constructor(private http: HttpClient) {}
 
   signUp(email: string, password: string) {
-    return this.http.post<AuthResponseData>(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBIuD7iNQOG8bvrGAjLvKUNdRTiLC7Et0k',
-      {
-        email: email,
-        password: password,
-        secureToken: true,
-      }
-    ).pipe(catchError(this.handleError));
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBIuD7iNQOG8bvrGAjLvKUNdRTiLC7Et0k',
+        {
+          email: email,
+          password: password,
+          secureToken: true,
+        }
+      )
+      .pipe(
+        catchError(this.handleError),
+        tap((resData) => {
+          this.handleAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          );
+        })
+      );
   }
 
   login(email: string, password: string) {
-    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBIuD7iNQOG8bvrGAjLvKUNdRTiLC7Et0k',
-    {
-      email: email,
-      password: password,
-      secureToken: true,
-    }).pipe(catchError(this.handleError));
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBIuD7iNQOG8bvrGAjLvKUNdRTiLC7Et0k',
+        {
+          email: email,
+          password: password,
+          secureToken: true,
+        }
+      )
+      .pipe(catchError(this.handleError));
   }
 
-  private handleError(errorRes: HttpErrorResponse){
-    let errorMessage = 'An unknown error occurred!'
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = 'An unknown error occurred!';
 
-    if(!errorRes.error || !errorRes.error.error){
+    if (!errorRes.error || !errorRes.error.error) {
       return throwError(errorMessage);
     }
-    switch( errorRes.error.error.message) {
+    switch (errorRes.error.error.message) {
       case 'EMAIL_EXISTS':
         errorMessage = 'This email exists already!';
         break;
@@ -57,6 +74,16 @@ export class AuthService {
         errorMessage = 'This password is not correct!';
     }
     return throwError(errorMessage);
+  }
 
+  private handleAuthentication(
+    email: string,
+    userId: string,
+    token: string,
+    expiresIn: number
+  ) {
+    const experationDate = new Date(new Date().getTime() + expiresIn * 1000);
+    const user = new User(email, userId, token, experationDate);
+    this.user.next(user);
   }
 }
