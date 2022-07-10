6ddf1611a59e632667e2dd36573b7e9836707766
Shane Reeve <shanereeve@gmail.com>
2022-07-10 10:51:42 -0600

    314: Preparing Programmatic Creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,6 +27,7 @@ import { AuthComponent } from './auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { AlertComponent } from './shared/alert/alert.component'
+import { PlaceholderDirective } from './shared/placeholder.directive';
 
 
 @NgModule({
@@ -50,7 +51,8 @@ import { AlertComponent } from './shared/alert/alert.component'
 
     AuthComponent,
     LoadingSpinnerComponent,
-    AlertComponent
+    AlertComponent,
+    PlaceholderDirective
   ],
   imports: [
     BrowserModule,
