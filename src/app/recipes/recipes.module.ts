86e014128875359dafd70773745ac1424d68fd48
Shane Reeve <shanereeve@gmail.com>
2022-07-11 17:33:34 -0600

    326: Component Declarations

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -1,9 +1,8 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { RouterModule } from "@angular/router";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { RecipesRoutingModule } from "./recipes-routing.module"
+import { RecipesRoutingModule } from './recipes-routing.module';
 
 import { RecipesComponent } from './recipes.component';
 import { RecipesListComponent } from './recipes-list/recipes-list.component';
@@ -12,30 +11,20 @@ import { RecipesItemComponent } from './recipes-list/recipes-item/recipes-item.c
 import { RecipeStartComponent } from './recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 
-
 @NgModule({
-  declarations:  [
-    RecipesComponent,
-    RecipesListComponent,
-    RecipesDetailComponent,
-    RecipesItemComponent,
-    RecipeStartComponent,
-    RecipeEditComponent
-  ],
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    RecipesRoutingModule
-    ],
-  exports: [
+    RecipesRoutingModule,
+  ],
+  declarations: [
     RecipesComponent,
     RecipesListComponent,
     RecipesDetailComponent,
     RecipesItemComponent,
     RecipeStartComponent,
-    RecipeEditComponent]
+    RecipeEditComponent,
+  ],
 })
-export class RecipesModule{
-
-}
+export class RecipesModule {}
