62c59ca35dcc20be07e0e99f657246abb8f156e2
Shane Reeve <shanereeve@gmail.com>
2022-07-12 15:29:52 -0600

    329: Understanding the Core Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS }from '@angular/common/http';
+import { HttpClientModule }from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
-import { ShoppingListService } from './shopping-list/shopping-list.service';
 
 import { OddListComponent } from './oddList/odd-list/odd-list.component';
 import { BasicHighlightDirective } from '../app/oddList/basic-highlight/basic-highlight.directive';
 import { BetterHighlightDirective } from './oddList/better-highlight/better-highlight.directive';
 import { UnlessDirective } from './oddList/unless.directive';
 
-import { RecipeService } from './recipes/recipe.service';
 import { AuthComponent } from './auth/auth.component';
-import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { AlertComponent } from './shared/alert/alert.component'
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core.module';
 
 
 @NgModule({
@@ -45,15 +43,10 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     RecipesModule,
     ShoppingListModule,
-    SharedModule
+    SharedModule,
+    CoreModule
   ],
-  providers: [
-    ShoppingListService,
-    RecipeService,
-    {provide: HTTP_INTERCEPTORS,
-    useClass: AuthInterceptorService,
-    multi: true}
-  ],
+
   // No Longer Needed Angular 9 or greater
   entryComponents: [
     AlertComponent
