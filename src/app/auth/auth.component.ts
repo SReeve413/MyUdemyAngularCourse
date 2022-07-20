23a27a5918ee6d262f84c52c8754a1793af5bffb
Shane Reeve <shanereeve@gmail.com>
2022-07-20 17:24:52 -0600

    382: Adding Signup

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,9 @@
-import { Component, ComponentFactoryResolver, OnInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  ComponentFactoryResolver,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Observable, Subscription } from 'rxjs';
@@ -29,15 +34,14 @@ export class AuthComponent implements OnInit {
     private store: Store<fromApp.AppState>
   ) {}
 
-
-  ngOnInit(){
-    this.store.select('auth').subscribe(authState => {
+  ngOnInit() {
+    this.store.select('auth').subscribe((authState) => {
       this.isLoading = authState.loading;
       this.error = authState.authError;
-      if(this.error){
+      if (this.error) {
         this.showErrorAlert(this.error);
       }
-    })
+    });
   }
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
@@ -54,30 +58,14 @@ export class AuthComponent implements OnInit {
 
     this.isLoading = true;
     if (this.isLoginMode) {
-      //...
-      // authObs = this.authService.login(email, password);
       this.store.dispatch(
         new AuthActions.LoginStart({ email: email, password: password })
       );
     } else {
-      authObs = this.authService.signUp(email, password);
+      this.store.dispatch(
+        new AuthActions.SignupStart({ email: email, password: password })
+      );
     }
-
-
-
-    // authObs.subscribe(
-    //   (responseData) => {
-    //     console.log(responseData);
-    //     this.isLoading = false;
-    //     this.router.navigate(['/recipes']);
-    //   },
-    //   (errorMessage) => {
-    //     console.log(errorMessage);
-    //     this.error = errorMessage;
-    //     this.showErrorAlert(errorMessage);
-    //     this.isLoading = false;
-    //   }
-    // );
 
     form.reset();
   }
