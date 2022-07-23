280f5658fb1174d96c983a909a72ecf5fe042f8b
Shane Reeve <shanereeve@gmail.com>
2022-07-22 20:24:57 -0600

    394: Storing Recipes via Effects

--- src/app/recipes/store/recipe.effects.ts
+++ src/app/recipes/store/recipe.effects.ts
@@ -1,7 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Actions, Effect, ofType } from '@ngrx/effects'
-import { map, switchMap } from 'rxjs/operators';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { Store } from '@ngrx/store';
+import { dispatch } from 'rxjs/internal/observable/pairs';
+import { map, switchMap, withLatestFrom } from 'rxjs/operators';
+import * as fromApp from 'src/app/store/app.reducer';
 import { Recipe } from '../recipe.model';
 
 import * as RecipesActions from './recipe.actions';
@@ -12,7 +15,9 @@ export class RecipeEffects {
   fetchRecipes = this.actions$.pipe(
     ofType(RecipesActions.FETCH_RECIPES),
     switchMap(() => {
-      return this.http.get<Recipe[]>('https://ng-course-recipe-book-41781-default-rtdb.firebaseio.com/recipes.json')
+      return this.http.get<Recipe[]>(
+        'https://ng-course-recipe-book-41781-default-rtdb.firebaseio.com/recipes.json'
+      );
     }),
     map((recipes) => {
       return recipes.map((recipe) => {
@@ -22,11 +27,22 @@ export class RecipeEffects {
         };
       });
     }),
-    map(recipes => {
-      return new RecipesActions.SetRecipes(recipes)
+    map((recipes) => {
+      return new RecipesActions.SetRecipes(recipes);
     })
-    )
+  );
 
+  @Effect({dispatch: false})
+  storeRecipes = this.actions$.pipe(
+    ofType(RecipesActions.STORE_RECIPES),
+    withLatestFrom(this.store.select('recipes')),
+    switchMap(([actionData, recipesState]) => {
+      return this.http.put(
+        'https://ng-course-recipe-book-41781-default-rtdb.firebaseio.com/recipes.json',
+        recipesState.recipes
+      );
+    })
+  );
 
-  constructor(private actions$: Actions, private http: HttpClient){}
+  constructor(private actions$: Actions, private http: HttpClient, private store: Store<fromApp.AppState>) {}
 }
